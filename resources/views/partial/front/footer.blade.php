435968e084fa8b3e81783922fe6e512f10b3e39d
Md Jisan Sheikh <rafijuljisan@gmail.com>
2026-01-08 16:16:24 +0600

    Footer changed

--- resources/views/partial/front/footer.blade.php
+++ resources/views/partial/front/footer.blade.php
@@ -1,124 +1,120 @@
+<style>
+
+</style>
 <footer class="footer-new">
   <div class="container jagaran-container">
     <div class="row custom-row">
-      <div class="col-lg-3 custom-padding">
-        <div class="contact-details">
-          <h3> <span> আমাদের পরিচিতি </span>  </h3>
-          <p>সম্পাদক ও প্রকাশক : {{$gs->sompadok}}</p>
-          <p>নির্বাহী সম্পাদক : {{$gs->nirbahi_sompadok}}</p>
-          <p>বার্তা সম্পাদক : {{$gs->barta_sompadok}}</p>
-        </div><!--/.contact-details-->
-        <ul class="social-icon footer-icon footer-icon-2">
-		
-		@foreach ($social_links as $social_link) 
-          <li><a target="_blank" href="{{ $social_link->link}}" class="{{$social_link->name}}"><i class="{{$social_link->icon}}"></i></a></li>
-         @endforeach
-        </ul><!--/.social-icon-->
-      </div><!--/.col-lg-3-->
-      <div class="col-lg-3 custom-padding">
-        <div class="contact-details">
-          <h3> <span> রেজিস্টার্ড অফিস </span>  </h3>
-          <ul class="footer-address-ul">
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-home" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-              {{$gs->address}}
-              </span>
-            </li>
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-              {{$gs->mobile}}
-              </span>
-            </li>
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-envelope-o" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-                {{$gs->email_address}}
-              </span>
-            </li>
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-internet-explorer" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-                {{ route('frontend.index')}}
-              </span>
-            </li>
-          </ul>
-        </div><!--/.contact-details-->
-      </div><!--/.col-md-4-->
-
-
-      <div class="col-lg-3 custom-padding">
-        <div class="contact-details">
-          <h3> <span> কর্পোরেট অফিস </span>  </h3>
-          <ul class="footer-address-ul">
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-home" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-              {{$gs->address}}
-              </span>
-            </li>
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-              {{$gs->mobile}}
-              </span>
-            </li>
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-envelope-o" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-               {{$gs->email_address}}
-              </span>
-            </li>
-            <li>
-              <span class="size-w-3">
-                <i class="fa fa-internet-explorer" aria-hidden="true"></i>
-              </span>
-              <span class="size-w-4">
-                {{ route('frontend.index')}}
-              </span>
-            </li>
-          </ul>
-        </div><!--/.contact-details-->
-      </div><!--/.col-md-3-->
-      <div class="col-lg-3 custom-padding">
-        <div class="contact-details">
-          <h3> <span> মানচিত্রে আমরা </span>  </h3>
-        </div>
-        <div class="footer-widget-map">
+      
+      <div class="col-lg-3 col-md-6 custom-padding">
+        <div class="footer-widget about-widget">
+          <h3 class="widget-title"> <span> আমাদের পরিচিতি </span> </h3>
           
-		  {!! $gs->google_map !!}
-		  
-        </div>
-      </div><!--/.col-lg-3-->
-    </div><!--/.row-->
-  </div><!--/.container-->
-</footer><!--/.footer-new-->
-<div class="footer">
-  <div class="container custom-container">
-    <div class="row custom-row footer-bottom-row">
-        <div class="col-md-8 footer-copy-text">
-          <p>{{$gs->copyright_text}}</p>
-        </div>
-        <div class="col-md-4">
-          <div class="design-link">
-            <p>সকল কারিগরী সহযোগিতায়  <a href="https://jisan.technomenia.com/" target="_blank" title="Jisan Sheikh"> Md Jisan</a></p>
+          <div class="editor-list">
+            @if($gs->sompadok)
+            <div class="editor-item">
+                <span class="role">সম্পাদক ও প্রকাশক</span>
+                <span class="name">{{$gs->sompadok}}</span>
+            </div>
+            @endif
+
+            @if($gs->nirbahi_sompadok)
+            <div class="editor-item">
+                <span class="role">নির্বাহী সম্পাদক</span>
+                <span class="name">{{$gs->nirbahi_sompadok}}</span>
+            </div>
+            @endif
+
+            @if($gs->barta_sompadok)
+            <div class="editor-item">
+                <span class="role">বার্তা সম্পাদক</span>
+                <span class="name">{{$gs->barta_sompadok}}</span>
+            </div>
+            @endif
+          </div>
+
+          <div class="social-area">
+             <p class="follow-text">সোশ্যাল মিডিয়া :</p>
+             <ul class="social-icon footer-social">
+                @foreach ($social_links as $social_link) 
+                  <li>
+                      <a target="_blank" href="{{ $social_link->link}}" class="{{$social_link->name}}">
+                          <i class="{{$social_link->icon}}"></i>
+                      </a>
+                  </li>
+                 @endforeach
+            </ul>
           </div>
         </div>
-    </div><!--/.custom-row-->
-  </div><!--/.custom-container-->
-</div><!--/.footer-->
+      </div>
+
+      <div class="col-lg-3 col-md-6 custom-padding">
+        <div class="footer-widget contact-widget">
+          <h3 class="widget-title"> <span> রেজিস্টার্ড অফিস </span> </h3>
+          <ul class="address-list">
+            <li>
+              <div class="icon-box"><i class="fa fa-map-marker"></i></div>
+              <div class="text-box">{{$gs->address}}</div>
+            </li>
+            <li>
+              <div class="icon-box"><i class="fa fa-phone"></i></div>
+              <div class="text-box">{{$gs->mobile}}</div>
+            </li>
+            <li>
+              <div class="icon-box"><i class="fa fa-envelope"></i></div>
+              <div class="text-box">{{$gs->email_address}}</div>
+            </li>
+            <li>
+              <div class="icon-box"><i class="fa fa-globe"></i></div>
+              <div class="text-box">{{ route('frontend.index')}}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 custom-padding">
+        <div class="footer-widget contact-widget">
+          <h3 class="widget-title"> <span> কর্পোরেট অফিস </span> </h3>
+          <ul class="address-list">
+             <li>
+              <div class="icon-box"><i class="fa fa-building"></i></div>
+              <div class="text-box">{{$gs->address}}</div>
+            </li>
+            <li>
+              <div class="icon-box"><i class="fa fa-phone"></i></div>
+              <div class="text-box">{{$gs->mobile}}</div>
+            </li>
+            <li>
+              <div class="icon-box"><i class="fa fa-envelope"></i></div>
+              <div class="text-box">{{$gs->email_address}}</div>
+            </li>
+             <li>
+              <div class="icon-box"><i class="fa fa-globe"></i></div>
+              <div class="text-box">{{ route('frontend.index')}}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 custom-padding">
+        <div class="footer-widget map-widget">
+          <h3 class="widget-title"> <span> মানচিত্রে আমরা </span> </h3>
+          <div class="google-map-wrapper">
+             {!! $gs->google_map !!}
+          </div>
+        </div>
+      </div>
+
+    </div></div></footer>
+
+<div class="footer-bottom">
+  <div class="container custom-container">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+          <p class="copyright-text">{{$gs->copyright_text}}</p>
+        </div>
+        <div class="col-md-6 text-right">
+          <p class="developer-text">কারিগরি সহযোগিতায়: <a href="https://jisan.technomenia.com/" target="_blank">Md Jisan</a></p>
+        </div>
+    </div>
+  </div>
+</div>
